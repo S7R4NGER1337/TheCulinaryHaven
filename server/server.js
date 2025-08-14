a148152e7c17623b3bdd0a076555dc5b06014e5e
S7R4NGER <nikolakoew@gmail.com>
2025-08-14 13:43:13 +0300

    getting all products and sending them as a response when get request is done to '/products'

--- server/server.js
+++ server/server.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 const mongoose = require('mongoose')
+const Product = require('./models/Product')
 
 app.use(express.urlencoded())
 app.use(express.json())
 
-router.get('/get', (req,res) => {
-    res.send('Hello')
+router.get('/products', async (req,res) => {
+    const allProducts = await Product.find()
+    
+    res.json(allProducts)
+    res.end()
 })
 
 mongoose.connect('mongodb://localhost:27017/TheCulinaryHaven')
