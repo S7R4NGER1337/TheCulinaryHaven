f746ffebcb4c0754af2e71b84b3fa2af76a5880d
S7R4NGER <nikolakoew@gmail.com>
2025-08-14 13:26:28 +0300

    adding category to product model

--- server/models/Product.js
+++ server/models/Product.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const ProductSchema = new mongoose.Schema({
     name: String,
     price: Number,
+    category: String,
     description: String,
     image: String
 })
