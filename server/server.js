ec5547aee1f0c8e9c120dcb146412e062b68a957
S7R4NGER <nikolakoew@gmail.com>
2025-08-14 13:24:46 +0300

    connecting db with the app

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const app = express()
 const router = express.Router()
-
+const mongoose = require('mongoose')
 
 app.use(express.urlencoded())
 app.use(express.json())
 
-router.get('/x', (req,res) => {
-    console.log(1);
-    
+router.get('/get', (req,res) => {
     res.send('Hello')
 })
+
+mongoose.connect('mongodb://localhost:27017/TheCulinaryHaven')
+    .then(() => console.log('Db connected'))
+    .catch(error => console.log(error))
 
 app.use(router)
 app.listen(3030, () => console.log('test'))
