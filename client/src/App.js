de804e371fa9c470411489739c5cee044f142327
S7R4NGER <nikolakoew@gmail.com>
2025-08-16 12:58:00 +0300

    adding flex direction to app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Banner from './components/Banner';
+import Menu from './components/Menu';
 
 function App() {
   return (
-    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
 
       <Banner />
+      <Menu />
     </div>
   );
 }
