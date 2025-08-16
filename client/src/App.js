20f8efdacd2773d80ea72b35f25db52b417b13eb
S7R4NGER <nikolakoew@gmail.com>
2025-08-16 13:19:16 +0300

    adding nav element to app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Banner from './components/Banner';
 import Menu from './components/Menu';
+import Nav from './components/Nav';
 
 function App() {
-  return (
+  return (<>
+      <Nav />
     <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-
       <Banner />
       <Menu />
     </div>
+  </>
   );
 }
 
