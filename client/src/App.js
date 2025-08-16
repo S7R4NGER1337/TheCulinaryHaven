ab6676790d14d27ded0c31cb565c728a9257d7dd
S7R4NGER <nikolakoew@gmail.com>
2025-08-16 13:44:27 +0300

    moving all css from app to a css module

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
+import styles from './app.module.css'
 import Banner from './components/Banner';
 import Menu from './components/Menu';
 import Nav from './components/Nav';
 
 function App() {
   return (<>
+    <div className={styles.navContainer}>
       <Nav />
-    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+      <div className={styles.navLine}/>
+    </div>
+
+    <div className={styles.pageContainer}>
       <Banner />
       <Menu />
     </div>
