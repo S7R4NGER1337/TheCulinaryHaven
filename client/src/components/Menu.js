642ba9f8349eef02318b0b8f3d64628b5b7c48c1
S7R4NGER <nikolakoew@gmail.com>
2025-08-17 13:44:23 +0300

    rendering products dynamically when category is changed

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,10 +1,29 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import MenuCatagoryElement from "./MenuCategoryElement"
 import styles from './menu.module.css'
+import ProductCard from "./ProductCard"
 
 export default function Menu() {
 
     const [selectedCategory, setSelectedCategory] = useState('Appetizers')
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        try {
+            async function getProducts() {
+                
+                const response = await fetch(`http://localhost:3030/products/category/${selectedCategory}`)
+                const products = await response.json()
+                
+                console.log(products);
+                setProducts(products);              
+            }
+            getProducts()
+        } catch (error) {
+            console.log(error);
+        }
+    }, [selectedCategory])
+
     const categories = [
         'Appetizers',
         'MainCourses',
@@ -21,6 +40,9 @@ export default function Menu() {
         <div className={styles.categoriesContainer}>
             {categories.map(category => <MenuCatagoryElement type={category} onClickCategory={onClickCategory} />)}
         </div>
-        <p>{selectedCategory}</p>
+        <h1 className={styles.selectedCategoryName}>{selectedCategory}</h1>
+        {products.map(((product) => (
+            <ProductCard key={product.id} productData={product}/>
+        )))}
     </div>
 }
