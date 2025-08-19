9fec70466ea5db6d34f9c09616203d8066c070f3
S7R4NGER <nikolakoew@gmail.com>
2025-08-19 13:19:44 +0300

    installing react-router-dom and making admin path

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Menu from './components/Menu';
 import Nav from './components/Nav';
 import AboutUs from './components/AboutUs'
 import { useRef } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Admin from './admin/Admin';
 
 function App() {
   const bannerRef = useRef(null)
@@ -11,24 +13,31 @@ function App() {
   const aboutUsRef = useRef(null)
 
   return (<>
-    <div className={styles.navContainer}>
-      <Nav bannerRef={bannerRef} menuRef={menuRef} aboutUsRef={aboutUsRef}/>
-      <div className={styles.navLine} />
-    </div>
 
-    <div className={styles.pageContainer}>
-      <div ref={bannerRef}>
-        <Banner menuRef={menuRef}/>
-      </div>
-      <div ref={menuRef}>
-        <Menu />
-      </div>
-      <h1 className={styles.aboutUsHeading}>AboutUs</h1>
-      <div style={{width: '100%'}} ref={aboutUsRef}>
-        <AboutUs />
-      </div>
-    </div>
-    <p className={styles.footer}>© 2025 The Culinary Haven. All rights reserved</p>
+    <Routes>
+      <Route path='/admin' element={<Admin />} />
+      <Route path='/' element={<>
+        <div className={styles.navContainer}>
+          <Nav bannerRef={bannerRef} menuRef={menuRef} aboutUsRef={aboutUsRef} />
+          <div className={styles.navLine} />
+        </div>
+        <div className={styles.pageContainer}>
+          <div ref={bannerRef}>
+            <Banner menuRef={menuRef} />
+          </div>
+          <div ref={menuRef}>
+            <Menu />
+          </div>
+          <h1 className={styles.aboutUsHeading}>AboutUs</h1>
+          <div style={{ width: '100%' }} ref={aboutUsRef}>
+            <AboutUs />
+          </div>
+        </div>
+        <p className={styles.footer}>© 2025 The Culinary Haven. All rights reserved</p>
+      </>
+      } />
+      <Route path='*' element={<h1>Page Not Found</h1>} />
+    </Routes>
   </>
   );
 }
