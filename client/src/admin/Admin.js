fa27e1feb7600b50150a21fff0bdef249ad4b92d
S7R4NGER <nikolakoew@gmail.com>
2025-08-19 13:44:52 +0300

    adding link to the app

--- client/src/admin/Admin.js
+++ client/src/admin/Admin.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react"
+import {useNavigate} from 'react-router-dom'
 import styles from './admin.module.css'
 import AdminProductCard from "./AdminProductCard"
 
 export default function Admin() {
     const [products, setProducts] = useState([])
+    const navigate = useNavigate()
 
     useEffect(() => {
         async function getAllProducts() {
@@ -17,8 +19,9 @@ export default function Admin() {
 
 
     return <>
+        <h1 className={styles.goBack} onClick={() => navigate('/')}>Back to the app</h1>
         <div className={styles.productsContainer}>
-            {products.map(product => <AdminProductCard productData={product} key={product.id}/>)}
+            {products.map(product => <AdminProductCard productData={product} key={product} />)}
         </div>
     </>
 }
