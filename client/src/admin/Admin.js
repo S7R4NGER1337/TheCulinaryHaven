2b44f95c600a6a2eb37fd5202b7ffe4dbe7338f1
S7R4NGER <nikolakoew@gmail.com>
2025-08-19 14:12:22 +0300

    adding loading animation before rendering products in admin page

--- client/src/admin/Admin.js
+++ client/src/admin/Admin.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react"
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import styles from './admin.module.css'
 import AdminProductCard from "./AdminProductCard"
+import Skeleton from '@mui/material/Skeleton';
+import Box from '@mui/material/Box';
 
 export default function Admin() {
     const [products, setProducts] = useState([])
+    const [loadStatus, setLoadStatus] = useState(true)
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -13,15 +16,25 @@ export default function Admin() {
             const products = await response.json()
 
             setProducts(products)
+            setLoadStatus(false)
         }
         getAllProducts()
     }, [])
 
+    const Loader = () => (
+        <Box sx={{ width: '90rem', margin: '3rem'}}>
+            <Skeleton animation="wave" sx={{height: '6rem'}}/>
+            <Skeleton animation="wave" sx={{height: '6rem'}}/>
+            <Skeleton animation="wave" sx={{height: '6rem'}}/>
+        </Box>
+    )
 
-    return <>
+    return (<>
         <h1 className={styles.goBack} onClick={() => navigate('/')}>Back to the app</h1>
-        <div className={styles.productsContainer}>
-            {products.map(product => <AdminProductCard productData={product} key={product} />)}
-        </div>
-    </>
+        {loadStatus ? <Loader /> : <>
+            <div className={styles.productsContainer}>
+                {products.map(product => <AdminProductCard productData={product} key={product} />)}
+            </div>
+        </>}
+    </>)
 }
