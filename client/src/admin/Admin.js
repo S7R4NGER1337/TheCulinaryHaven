0c9ae2b567843f60301506d14cd456e40e2c3f68
S7R4NGER <nikolakoew@gmail.com>
2025-08-19 13:21:06 +0300

    creating admin page

---
+++ client/src/admin/Admin.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+import styles from './admin.module.css'
+import AdminProductCard from "./AdminProductCard"
+
+export default function Admin() {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        async function getAllProducts() {
+            const response = await fetch('http://localhost:3030/products')
+            const products = await response.json()
+
+            setProducts(products)
+        }
+        getAllProducts()
+    }, [])
+
+    console.log(products);
+    
+    return <div className={styles.productsContainer}>
+        {products.map(product => <AdminProductCard productData={product}/>)}
+    </div>
+}
