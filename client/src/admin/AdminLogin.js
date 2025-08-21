cda7b29aad3e2d121c0725f5880acf8fdf29849a
S7R4NGER <nikolakoew@gmail.com>
2025-08-21 20:23:14 +0300

    sending login data to the server

--- client/src/admin/AdminLogin.js
+++ client/src/admin/AdminLogin.js
@@ -1,19 +1,44 @@
+import { useState } from 'react'
 import styles from './adminLogin.module.css'
+import { useNavigate } from 'react-router-dom'
 
 export default function AdminLogin() {
+    const [loginData, setLoginData] = useState({
+        userName: '',
+        password: ''
+    })
+    const navigate = useNavigate()
 
+    async function loginSubmit() {
+        if(loginData.userName === '' || loginData.password === '') return
+
+        const response = await fetch('http://localhost:3030/admin/login', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(loginData)
+        })
+
+        const authToken = await response.json()
+        if(authToken !== ''){
+            localStorage.setItem('token', authToken.adminToken)
+            navigate('/admin')
+        }
+        
+    }
     return (
         <div className={styles.loginContainer}>
             <h1 className={styles.loginHeading}>ADMIN LOGIN</h1>
             <div className={styles.inputContainer}>
                 <label>Name</label>
-                <input className={styles.input} />
+                <input className={styles.input} value={loginData.userName} onChange={(e) => setLoginData({...loginData, userName: e.target.value})}/>
             </div>
             <div className={styles.inputContainer}>
                 <label>Password</label>
-                <input className={styles.input} />
+                <input className={styles.input} value={loginData.password} onChange={(e) => setLoginData({...loginData, password: e.target.value})}/>
             </div>
-            <button className={styles.loginButton}>Login</button>
+            <button className={styles.loginButton} onClick={() => loginSubmit()}>Login</button>
         </div>
     )
 }
