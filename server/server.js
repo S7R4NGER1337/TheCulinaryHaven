47128d7f8d3f07dcfdf0a0127ab0a01ba95ebb45
S7R4NGER <nikolakoew@gmail.com>
2025-08-22 18:28:15 +0300

    adding new cors setting

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ const bcrypt = require('bcrypt')
 
 app.use(express.urlencoded())
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
 app.use(cookieParser());
 
 function verifyAdmin(req, res, next) {
